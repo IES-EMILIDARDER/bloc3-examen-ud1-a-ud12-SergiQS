8aa46622a9ec0fd2163444f2efbd0c8b54d7886c
SergiQS <squetglass@gmail.com>
2025-05-19 15:46:08 +0200

    Main 5

--- EXAMEN/src/examen/Main5.java
+++ EXAMEN/src/examen/Main5.java
@@ -1,8 +1,51 @@
 package examen;
+import examen.Vehicle;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class Main5 {
     
     public static void main(String[] args) {
+        
+         final String MYSQL_CON = "c:\\temp\\mysql.con";
+        GestorBBDD gestorBBDD = new GestorBBDD(MYSQL_CON);
+
+        try (Stream<String> linies = Files.lines(Paths.get("c:\\temp\\vehicles.csv")); Connection conn = gestorBBDD.getConnectionFromFile()) {
+            linies.filter(linia -> !linia.isBlank() && !linia.startsWith("#")).map(linia -> linia.split(","))
+                    .forEach(parts -> {
+                        try {
+                            gestorBBDD.executaSQL(conn, "INSERT INTO vehicles (matricula, marca, model, any, preu) VALUES (?, ?, ?, ?, ? )", parts[0].trim(), parts[1].trim(), parts[2].trim(), parts[3].trim(), Integer.parseInt(parts[4].trim()));
+                        } catch (SQLException e) {
+                              try {
+                                    if ((e.getSQLState().equals("23000") && e.getErrorCode() == 1062)){
+                                        // si la primary key esta repetida hacemos un update
+                                        String  matricula = parts[0].trim();
+                                        String marca = parts[1].trim();
+                                        String model = parts[2].trim();
+                                        String any =parts[3].trim() ;
+                                        double preu = Integer.parseInt(parts[4].trim());
+                                        
+                                        gestorBBDD.executaSQL(conn,"UPDATE vehicles SET matricula = ?, marca = ? , model = ? , any = ? , preu = ?  ", matricula, marca, model , any, preu);
+                                        
+                                    }else {
+                                        throw new RuntimeException(e);
+                                    }
+                              }catch (SQLException e2){
+                                  throw new RuntimeException(e2);
+                                  
+                              }
+                        }
+                    });
+
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
 
     }
-}
+
+    }
+
--- EXAMEN/src/examen/Vehicle.java
+++ EXAMEN/src/examen/Vehicle.java
@@ -1,5 +1,7 @@
 package examen;
 
+import java.util.Objects;
+
 class Vehicle {
     private String matricula;
     private String marca;
@@ -15,4 +17,68 @@ class Vehicle {
         this.preu = preu;
     }
 
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.matricula);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Vehicle other = (Vehicle) obj;
+        return Objects.equals(this.matricula, other.matricula);
+    }
+    
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getAny() {
+        return any;
+    }
+
+    public void setAny(int any) {
+        this.any = any;
+    }
+
+    public double getPreu() {
+        return preu;
+    }
+
+    public void setPreu(double preu) {
+        this.preu = preu;
+    }
+    
+    
 }
